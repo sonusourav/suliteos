8312d772f2adb8f89a231e26812a0397865e78e3
sonusourav <34706326+sonusourav@users.noreply.github.com>
2018-07-05 12:42:38 +0530

    Update ForgotPassword.java

--- app/src/main/java/com/suliteos/sonusourav/poshan/ForgotPassword.java
+++ app/src/main/java/com/suliteos/sonusourav/poshan/ForgotPassword.java
@@ -1,10 +1,14 @@
 package com.suliteos.sonusourav.poshan;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ForgotPassword extends AppCompatActivity {
 
+    private ActionBar forgotActionBar;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +18,28 @@ public class ForgotPassword extends AppCompatActivity {
             onRestoreInstanceState(savedInstanceState);
         }
     }
+    public boolean onCreateOptionsMenu(Menu menu) {
 
+        forgotActionBar = getSupportActionBar();
+        assert forgotActionBar != null;
+        forgotActionBar.setHomeButtonEnabled(true);
+        forgotActionBar.setDisplayHomeAsUpEnabled(true);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+
+            case android.R.id.home:
+                this.finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
 }
